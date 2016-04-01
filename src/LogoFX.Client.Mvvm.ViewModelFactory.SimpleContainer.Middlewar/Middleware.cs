b7f94659bac669ffc78e1215ce5d88486bbb691e
godrose <godrose@gmail.com>
2016-04-01 19:38:59 +0300

    Added middleware project.

---
+++ src/LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Middlewar/Middleware.cs
@@ -0,0 +1,44 @@
+﻿using LogoFX.Client.Bootstrapping;
+using LogoFX.Client.Bootstrapping.Adapters.SimpleContainer;
+using LogoFX.Client.Mvvm.ViewModel.Contracts;
+using Solid.Practices.Middleware;
+
+namespace LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer
+{
+    /// <summary>
+    /// Middleware that registers view model factory implemented using LogoFX Simple Container.
+    /// </summary>
+    /// <typeparam name="TRootViewModel">The type of the root view model.</typeparam>    
+    public class RegisterViewModelFactoryMiddleware<TRootViewModel> : 
+        IMiddleware<IBootstrapperWithContainerAdapter<TRootViewModel, ExtendedSimpleContainerAdapter>>        
+    {
+        /// <summary>
+        /// Applies the middleware on the specified object.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <returns></returns>
+        public IBootstrapperWithContainerAdapter<TRootViewModel, ExtendedSimpleContainerAdapter> 
+            Apply(IBootstrapperWithContainerAdapter<TRootViewModel, ExtendedSimpleContainerAdapter> @object)
+        {
+            @object.ContainerAdapter.RegisterSingleton<IViewModelFactory, ViewModelFactory>();
+            return @object;
+        }
+    }
+
+    /// <summary>
+    /// Bootstrapper extensions.
+    /// </summary>
+    public static class BootstrapperExtensions
+    {
+        /// <summary>
+        /// Uses the view model factory which is based on LogoFX Simple Container.
+        /// </summary>
+        /// <typeparam name="TRootViewModel">The type of the root view model.</typeparam>
+        /// <param name="bootstrapper">The bootstrapper.</param>
+        public static void UseViewModelFactory<TRootViewModel>(
+            this IBootstrapperWithContainerAdapter<TRootViewModel, ExtendedSimpleContainerAdapter> bootstrapper)
+        {
+            bootstrapper.Use(new RegisterViewModelFactoryMiddleware<TRootViewModel>());
+        }
+    }
+}
