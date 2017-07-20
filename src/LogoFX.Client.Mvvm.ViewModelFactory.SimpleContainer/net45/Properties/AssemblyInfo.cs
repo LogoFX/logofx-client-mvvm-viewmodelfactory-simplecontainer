0bd22427f7f306f5d28bca087b3019f91e8059e9
Gennady Verdel <godrose@gmail.com>
2017-07-21 00:20:09 +0300

    Updated packages.

--- src/LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer/net45/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer/net45/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.NET45")]
+[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.NET45")]
+[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
