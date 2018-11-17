bc38924618fd991f6eb3cc884e664a3d4a978fcc
Gennady Verdel <godrose@gmail.com>
2018-11-17 13:02:08 +0200

    Dropped .NETStrandard20 suffix

--- src/LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests/Properties/AssemblyInfo.cs
@@ -1,34 +1,34 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests")]
+[assembly: AssemblyTitle("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests.NET20")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("LogoFX.Client.Mvvm.ViewModelFactory.SimpleContainer.Tests.NET20")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ab7232e-0dad-415b-87d2-26974a776eb7")]
+[assembly: Guid("75d2f5a7-ad38-4c27-b32a-cf0c22c70954")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
